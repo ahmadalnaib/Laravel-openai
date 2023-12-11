3f2050b86c47e9a5f6ef33367967153f0f1b8501
neu <alnaib888@gmail,com>
2023-12-11 22:01:51 +0100

    add ai

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+   $response= Http::withToken(config('services.openai.secret'))->post('https://api.openai.com/v1/chat/completions', [
+        "model" => "gpt-3.5-turbo",
+        "messages" => [
+            [
+                "role" => "system",
+                "content" => "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."
+            ],
+            [
+                "role" => "user",
+                "content" => "Compose a poem that explains the concept of recursion in programming."
+            ]
+        ]
+    ])->json('choices.0.message.content');
+  return view('welcome',[
+      'response' => $response
+  ]);
+   
 });
